997a5c9a3b8eb1dd13331bf0c7067999c38b6d48
Cole Garvey <colemiester19@outlook.com>
2024-06-08 23:35:41 -0400

    included key for successful axios call

--- client/src/helpers/initHeadlines.js
+++ client/src/helpers/initHeadlines.js
@@ -1,20 +1,25 @@
 import axios from 'axios';
 
 const init_Headlines = async () => {
-    const akey = process.env.REACT_APP_API_KEY;
+    let config = {
+        headers : {
+            'X-Api-Key': process.env.REACT_APP_API_KEY,
+        }
+    }
     const headlines = await 
-    axios.get(`https://newsapi.org/v2/top-headlines/sources?country=usapiKey=${akey}`)
+    axios.get(`https://newsapi.org/v2/top-headlines/sources?country=us`, config)
     .then(data => {
-        if (data.data.status !== 'ok') {
+        if (data.status !== 200) {
             return {message: "Error fetching headlines"};
+        } else {
+            // console.log(data);
+            return data.data.sources;
         }
     })
     .catch(error => {
         return error;
     });
-
-    return  headlines;
-
+    // console.log(headlines);
 };
 
 export default init_Headlines;
